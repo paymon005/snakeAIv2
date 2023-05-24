c49b0e21f97f539ef0b14245010da739a5ccc1ff
paymon005 <75635414+paymon005@users.noreply.github.com>
2023-05-23 20:54:45 -0700

    updated layers file
    added ability to print some tensorboard data to PDF
    
    moved add hidden layer to method

--- DeepNeuralNetworkDriver.py
+++ DeepNeuralNetworkDriver.py
@@ -2,16 +2,21 @@ import time
 import numpy as np
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
-from tflearn import callbacks, conv_2d, conv_1d
+from tflearn import callbacks, conv_2d, conv_1d, max_pool_2d
 import tflearn
 import os
 import tensorflow as tf
 import pickle
+from tqdm import tqdm
 import MyTools
 import tensorflow.python.util.deprecation as deprecation
-
+import matplotlib
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import pyplot as plt
+from tbparse import SummaryReader
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 deprecation._PRINT_DEPRECATION_WARNINGS = False
+matplotlib.use('Agg')
 
 
 class DnnDriver:
@@ -27,14 +32,14 @@ class DnnDriver:
         self.model_name = model_name
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.layer_types =          ['conv_2d', 'conv_2d', 'fully_connected']  # conv_2d, fully_connected, conv_1d
-        self.layer_nodes =          [None, None, 256]  # number of nodes in each core layer
-        self.layer_num_of_filters = [16, 32, None]  # only for conv layers
-        self.layer_filter_sizes =   [[3, 3], [2, 2], None]  # only for conv layers
-        self.layer_strides =        [[4, 4], [2, 2], None]  # only for conv layers
-        self.dropouts =             [False, False, True]  # whether to add a dropout on this layer
-        self.keep_probability =     [None,  None,   0.9]  # dropout keep rate
-        self.activations =          ['linear', 'linear', 'linear']  # each layers activation function
+        # all arrays below must be the same length (fill None at idxs where values do not apply)
+        self.layer_types = ['conv_2d', 'max_pool_2d', 'fully_connected']  # conv_2d, fully_connected, conv_1d
+        self.layer_num_of_elements = [32, [2, 2], 20]  # number of layer elements
+        self.layer_filter_sizes = [8, None, None]  # only for conv layers
+        self.layer_strides = [1, [2, 2], None]  # only for conv layers
+        self.dropouts = [False, False, True]  # whether to add a dropout on this layer
+        self.keep_probability = [None, None, 0.9]  # dropout keep rate
+        self.activations = ['relu', None, 'linear']  # each layers activation function
         self.output_layer_activation = 'softmax'
         self.regression_optimizer = 'adam'
         self.loss_function = 'mean_square'
@@ -61,10 +66,10 @@ class DnnDriver:
 
     def train_model(self):
         self.save_layers()
-        # input_length = len(self.training_data[0][0])
         if self.layer_types[0] == 'conv_2d':
             X = np.array([i[0] for i in self.training_data]).reshape((-1, self.game_size[1], self.game_size[0], 1))
         else:
+            # input_length = len(self.training_data[0][0])
             input_length = self.training_data[0][0].size
             X = np.array([i[0] for i in self.training_data]).reshape((-1, input_length, 1))
         y = np.array([i[1] for i in self.training_data]).reshape(-1, self.output_size)
@@ -84,40 +89,17 @@ class DnnDriver:
             tf.profiler.experimental.stop()
 
     def neural_network_model(self):
-        tnorm = tflearn.initializations.uniform(minval=-1.0, maxval=1.0)
         tflearn.init_graph(num_cores=self.num_of_cores, gpu_memory_fraction=self.gpu_mem)
 
         if self.layer_types[0] == 'fully_connected' or self.layer_types[0] == 'conv_1d':
             self.network = input_data(shape=[None, self.observation_space_length, 1], name='input')
         else:
             self.network = input_data(shape=[None, self.game_size[1], self.game_size[0], 1], name='input')
-
-        for i in range(0, len(self.layer_types)):
-            if self.layer_types[i] == 'fully_connected':
-                self.network = fully_connected(self.network,
-                                               n_units=self.layer_nodes[i],
-                                               activation=self.activations[i],
-                                               weights_init=tnorm)
-            elif self.layer_types[i] == 'conv_2d':
-                self.network = conv_2d(self.network,
-                                       nb_filter=self.layer_num_of_filters[i],
-                                       filter_size=self.layer_filter_sizes[i],
-                                       strides=self.layer_strides[i],
-                                       activation=self.activations[i],
-                                       weights_init=tnorm)
-            elif self.layer_types[i] == 'conv_1d':
-                self.network = conv_1d(self.network,
-                                       nb_filter=self.layer_num_of_filters[i],
-                                       filter_size=self.layer_filter_sizes[i],
-                                       strides=self.layer_strides[i],
-                                       activation=self.activations[i],
-                                       weights_init=tnorm)
-            if self.dropouts[i]:
-                self.network = dropout(self.network, self.keep_probability[i])
-
+        self.add_hidden_layers()
         self.network = fully_connected(self.network, self.output_size, activation=self.output_layer_activation)
         self.network = regression(self.network, optimizer=self.regression_optimizer, learning_rate=self.learning_rate,
                                   loss=self.loss_function, name='targets')
+
         if self.save_checkpoints and self.model_dir is not None:
             checkpoint_dir = os.path.join(self.model_dir, self.run_dir, self.model_name)
             best_checkpoint_dir = os.path.join(self.model_dir, self.run_dir, 'best-' + self.model_name)
@@ -126,6 +108,36 @@ class DnnDriver:
         else:
             self.model = tflearn.DNN(self.network, tensorboard_dir=self.log_dir)
         return self.model
+
+    def add_hidden_layers(self):
+        tnorm = tflearn.initializations.uniform(minval=-1.0, maxval=1.0)
+        for i in range(0, len(self.layer_types)):
+            if self.layer_types[i] == 'fully_connected':
+                self.network = fully_connected(self.network,
+                                               n_units=self.layer_num_of_elements[i],
+                                               activation=self.activations[i],
+                                               weights_init=tnorm)
+            elif self.layer_types[i] == 'conv_2d':
+                self.network = conv_2d(self.network,
+                                       nb_filter=self.layer_num_of_elements[i],
+                                       filter_size=self.layer_filter_sizes[i],
+                                       strides=self.layer_strides[i],
+                                       activation=self.activations[i],
+                                       weights_init=tnorm)
+            elif self.layer_types[i] == 'conv_1d':
+                self.network = conv_1d(self.network,
+                                       nb_filter=self.layer_num_of_elements[i],
+                                       filter_size=self.layer_filter_sizes[i],
+                                       strides=self.layer_strides[i],
+                                       activation=self.activations[i],
+                                       weights_init=tnorm)
+            elif self.layer_types[i] == 'max_pool_2d':
+                self.network = max_pool_2d(self.network,
+                                           kernel_size=self.layer_num_of_elements[i],
+
+                                           )
+            if self.dropouts[i]:
+                self.network = dropout(self.network, self.keep_probability[i])
 
     def save_model(self):
         self.model.save(self.model_dir + '\\' + self.run_dir)
@@ -164,8 +176,10 @@ class DnnDriver:
     def plot_graphs(self):
         os.system("taskkill /IM ""tensorboard.main"" /F")
         os.system("taskkill /IM ""tensorboard.exe"" /F")
-        print(os.getcwd() + "\\venv\\Scripts\\python.exe -m tensorboard.main --logdir=" + self.log_dir)
-        os.system(os.getcwd() + "\\venv\\Scripts\\python.exe -m tensorboard.main --logdir=" + self.log_dir)
+        print(os.getcwd() + "\\venv\\Scripts\\python.exe -m tensorboard.main --logdir=" + self.log_dir +
+              '--samples_per_plugin scalars=9999999999,images=9999999999')
+        os.system(os.getcwd() + "\\venv\\Scripts\\python.exe -m tensorboard.main --logdir=" + self.log_dir +
+                  '--samples_per_plugin scalars=9999999999,images=9999999999')
         #  print("tensorboard. --logdir " + self.log_dir)
         #  os.system("tensorboard --logdir " + self.log_dir)
 
@@ -190,14 +204,13 @@ class DnnDriver:
     def save_layers(self):
         data = {}
         file = open(os.path.join(self.model_dir, self.run_dir, 'Model_Network.pkl'), 'wb')
-        data['layer_nodes'] = self.layer_nodes
         data['activations'] = self.activations
         data['dropouts'] = self.dropouts
         data['output_layer_activation'] = self.output_layer_activation
         data['regression_optimizer'] = self.regression_optimizer
         data['loss_function'] = self.loss_function
         data['layer_types'] = self.layer_types
-        data['layer_num_of_filters'] = self.layer_num_of_filters
+        data['layer_num_of_elements'] = self.layer_num_of_elements
         data['layer_filter_sizes'] = self.layer_filter_sizes
         data['layer_strides'] = self.layer_strides
         data['observation_space_length'] = self.observation_space_length
@@ -208,26 +221,24 @@ class DnnDriver:
     def load_layers(self):
         path = os.path.join(self.model_dir, self.run_dir, 'Model_Network.pkl')
         if os.path.isfile(path):
-            # try:
+            try:
                 file = open(path, 'rb')
                 data = pickle.load(file)
-                self.layer_nodes = data['layer_nodes']
                 self.activations = data['activations']
                 self.dropouts = data['dropouts']
                 self.output_layer_activation = data['output_layer_activation']
                 self.regression_optimizer = data['regression_optimizer']
                 self.loss_function = data['loss_function']
                 self.layer_types = data['layer_types']
-                self.layer_num_of_filters = data['layer_num_of_filters']
+                self.layer_num_of_elements = data['layer_num_of_elements']
                 self.layer_filter_sizes = data['layer_filter_sizes']
                 self.layer_strides = data['layer_strides']
                 self.observation_space_length = data['observation_space_length']
                 self.keep_probability = data['keep_probability']
                 file.close()
                 print('Loading: ' + path)
-            # except:
-            #     print('Could not load model, trying defined model...')
-            #     time.sleep(1)
+            except KeyError:
+                raise Exception('Outdated model version, does not contain required data for network generation!')
         else:
             print('No Model Network to load!')
             time.sleep(1)
@@ -242,6 +253,61 @@ class DnnDriver:
             print('Found GPU at: {}'.format(device_name))
             # gpus = tf.config.list_physical_devices('GPU')
             # tf.config.set_visible_devices(gpus[0], 'GPU')
+
+    def print_tensorboard_plots(self, plot_scalars=True, plot_histograms=False):
+        tmp = os.path.join(self.log_dir, self.run_dir)
+        try:
+            log_path = os.path.join(tmp, os.listdir(tmp)[0])
+            print('Loading log: ' + log_path)
+        except FileNotFoundError:
+            print('No log data in dir: ' + tmp)
+            return
+        pdf = PdfPages(os.path.join(self.model_dir, self.run_dir, "TrainingPlots.pdf"))
+        start_time = time.time()
+        reader = SummaryReader(log_path)
+        print(time.time() - start_time)
+        if plot_scalars:
+            df_scalars = reader.scalars
+            self.plot_scalars(pdf, df_scalars)
+            del df_scalars
+        if plot_histograms:
+            df_histograms = reader.histograms
+            self.plot_histograms(pdf, df_histograms)
+        pdf.close()
+
+    @staticmethod
+    def plot_histograms(pdf, df):  # takes 400 years, not even worth
+        tags = df.tag.unique()
+        for tag in tags:
+            print('Plotting ' + tag + ' Histogram')
+            limits = df[df['tag'] == tag].limits
+            counts = df[df['tag'] == tag].counts
+            steps = df[df['tag'] == tag].step
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d', rasterized=True)
+            for i in tqdm(range(0, len(df.step))):
+                ax.bar(limits[i], counts[i], zs=steps[i], zdir='z', color='r', ec='k', alpha=0.4)
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            pdf.savefig(fig)
+            plt.clf()
+            plt.close('all')
+
+    @staticmethod
+    def plot_scalars(pdf, df):
+        tags = df.tag.unique()
+        for tag in tags:
+            print('Plotting ' + tag)
+            values = df[df['tag'] == tag]['value']
+            fig = plt.figure()
+            plt.plot(values)
+            plt.xlabel('Step')
+            plt.grid()
+            plt.title(tag)
+            pdf.savefig(fig)
+            plt.clf()
+            plt.close('all')
 
 
 class EarlyStoppingCallback(callbacks.Callback):
